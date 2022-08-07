29f31ac20a33a0bbd7f982cfb83e44a05533b1e5
JR-Camacho <josecamachoc0303@gmailcom>
2022-08-07 01:18:02 -0400

    Eliminacion de los componentes de sesiones y cambio de fuentes y color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,6 @@ import { GraficosComponent } from './graficos/graficos.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PersonalComponent } from './personal/personal.component';
-import { Sesion1EstudioComponent } from './sesion1-estudio/sesion1-estudio.component';
-import { Sesion1ExteriorComponent } from './sesion1-exterior/sesion1-exterior.component';
-import { Sesion2EstudioComponent } from './sesion2-estudio/sesion2-estudio.component';
-import { Sesion2ExteriorComponent } from './sesion2-exterior/sesion2-exterior.component';
-import { Sesion3EstudioComponent } from './sesion3-estudio/sesion3-estudio.component';
-import { Sesion3ExteriorComponent } from './sesion3-exterior/sesion3-exterior.component';
-import { Sesion4EstudioComponent } from './sesion4-estudio/sesion4-estudio.component';
-import { Sesion4ExteriorComponent } from './sesion4-exterior/sesion4-exterior.component';
-import { Sesion5EstudioComponent } from './sesion5-estudio/sesion5-estudio.component';
-import { Sesion6EstudioComponent } from './sesion6-estudio/sesion6-estudio.component';
 import { SesionesEstudioComponent } from './sesiones-estudio/sesiones-estudio.component';
 import { SesionesExteriorComponent } from './sesiones-exterior/sesiones-exterior.component';
 import { SesionesComponent } from './sesiones/sesiones.component';
@@ -44,36 +34,6 @@ const routes: Routes = [
   }, 
   {
     path: 'sesiones_estudio', component:SesionesEstudioComponent
-  },
-  {
-    path: 'sesion1_estudio', component:Sesion1EstudioComponent
-  },
-  {
-    path: 'sesion2_estudio', component:Sesion2EstudioComponent
-  },
-  {
-    path: 'sesion3_estudio', component:Sesion3EstudioComponent
-  },
-  {
-    path: 'sesion4_estudio', component:Sesion4EstudioComponent
-  },
-  {
-    path: 'sesion5_estudio', component:Sesion5EstudioComponent
-  },
-  {
-    path: 'sesion6_estudio', component:Sesion6EstudioComponent
-  },
-  {
-    path: 'sesion1_exterior', component:Sesion1ExteriorComponent
-  },
-  {
-    path: 'sesion2_exterior', component:Sesion2ExteriorComponent
-  },
-  {
-    path: 'sesion3_exterior', component:Sesion3ExteriorComponent
-  },
-  {
-    path: 'sesion4_exterior', component:Sesion4ExteriorComponent
   },
   {
     path: '**', component:PageNotFoundComponent
